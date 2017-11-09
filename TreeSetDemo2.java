94fae97f75c8de6f99e2382431bbe55fe21a23eb
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-11-09 23:26:43 +0530

    To illustrate the concept of TreeSet in java.

---
+++ TreeSetDemo2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class  TreeSetDemo1
+{
+  public static void main(String[] args)
+  {
+     /*
+     TreeSet t=new TreeSet();
+     t.add(new StringBuffer("A"));
+     t.add(new StringBuffer("B"));
+     t.add(new StringBuffer("C"));
+     t.add(new StringBuffer("D"));
+     System.out.println(t);
+     */
+	 
+		
+	/*Objects should be Homogeneous and Comparable if are using Default Natural Sorting Order
+		otherwise RE: ClassCastException will occur.*/
+  }
+}
