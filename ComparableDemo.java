fb7860bf0c6b3c0cdc3c51dd21b0216d2966e9fa
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-11-10 23:15:55 +0530

    To illustrate the concept of Comparable in java.

---
+++ ComparableDemo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class  ComparableDemo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("A".compareTo("Z"));
+		System.out.println("Z".compareTo("A"));
+		System.out.println("A".compareTo("A"));
+
+		//System.out.println("A".compareTo(null));  -> RE: NullPointerException
+	}
+}
